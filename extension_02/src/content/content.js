711087a01d312425407d8b2231b720d0a1e0b879
Denis Vershadov <day85@tut.by>
2019-09-27 10:01:40 +0300

    Added extension 02.

---
+++ extension_02/src/content/content.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+    document.body.innerHTML = document.body.innerHTML.replace(/google/ig, 'Mozilla');
+    document.body.innerHTML = document.body.innerHTML.replace(/chrome/ig, 'Firefox');
+};
